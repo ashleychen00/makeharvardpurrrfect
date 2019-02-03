e6cc35da1d98e7a76c2ba360dacc9f5b7639fc98
ashleychen00 <36568751+ashleychen00@users.noreply.github.com>
2019-02-03 00:48:58 -0500

    Add files via upload

--- makeharvard/site.js
+++ makeharvard/site.js
@@ -5,6 +5,8 @@ function setAlarm() {
     submitAlarmTime.addEventListener("click", function() {
         const setTime = setAlarmTime.value;
         newAlarmDOM(setTime);
+        newAlarmPost(setTime);
+        console.log(setTime);
     });
 };
 
@@ -15,12 +17,21 @@ function newAlarmDOM(time) {
     newAlarmItem.innerText = time;
 };
 
+function newAlarmPost(time) {
+    post("http://192.168.43.50:8080/time", time, function() {
+        console.log("we successfully set the alarm for: " + time);
+    });
+
+    /* $.post("192.168.43.50:8080/time", time, function() {
+        console.log("we successfully sent the alarm at: " + time);
+    }); */
+};
+
 setAlarm();
 
 const alarmContainer = document.getElementById("alarm-container");
-console.log(alarmContainer.innerHTML !== "");
 if(alarmContainer.innerHTML !== "") {
     const emptyAlarm = document.createElement("li");
     alarmContainer.appendChild(emptyAlarm);
     emptyAlarm.innerText = "You haven't set an alarm yet. Set one now!";
-}
+};
